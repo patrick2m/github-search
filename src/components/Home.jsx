3dc64d63d40b950c398f595872eb9b4cc7d883df
patrick2m <pmm12@hotmail.com>
2023-03-13 11:36:30 -0300

    criada função que verifica se existem respostas da procura por usuário

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,65 +2,74 @@ import { useState } from 'react';
 
 import './Home.css';
 
-import gitlogo from '../assets/Vector.png'
+import gitlogo from '../assets/Vector.png';
+import axios from 'axios';
 
 export default function Home() {
-  // const [ buscaAprovada, setBuscaAprovada ] = useState(false);
-
-  //  Função que retornar a quantidade de items encontrados para dar erro ou redirecionar para a página que mostra os usuários
-  //  function verificaBusca() {
-  //    axios.get(`https://api.github.com/search/users?q=${usuario}`).then(response => {
-  //    if (response.data.total_count > 0) {
-  //      setBuscaAprovada(true)
-  //    } else {
-  //      setBuscaAprovada(false)
-  //    }
-  //    { setBuscaAprovada ? 
-  //      ir para a página com o nome o usuário : 
-  //      Abrir modal de erro
-  //     }
-  //    })
-  //  }
+  const [ tipo, setTipo ] = useState('');
 
   // Função que retornar a quantidade de items encontrados para dar erro ou redirecionar para a página que mostra os usuários
-  // function verificaBusca() {
-  //   axios.get(`https://api.github.com/search/users?q=${usuario}`).then(response => {
-  //   setUsers(response.data.total_count)
-  //   console.log(users)
-  //   })
-  // }
+  async function verificarUsers(usuario) {
+    await axios.get(`https://api.github.com/search/users?q=${usuario}`).then(response => {
+      console.log(response)
+      { response.data.total_count > 0 ? 
+        (
+          console.log('Tem respostas') 
+        ) : (
+          console.log('Não tem respostas')
+        )
+      }
+    })
+  }
+
+  function verificarRepos(repositorio) {
+    axios.get(`https://api.github.com/search/users?q=${repositorio}`).then(response => {
+
+    })
+  }
+
+  function handleButtonClick(tipo, tipoalternado){
+    setTipo(tipo)
+    document.getElementById(tipo).classList.add("botaoSelecionado")
+    document.getElementById(tipoalternado).classList.remove("botaoSelecionado")
+  }
 
   function handleSubmit(e){
     e.preventDefault()
-    //verificar busca:
-    // if (e.tipo ==== 'repos') { verificarRepos(e.input.value) }
-    // if (e.tipo ==== 'users') { verificarUsers(e.input.value) }
-
-    console.log(e)
-
-    return {
-    }
+  //    Verificar busca:
+    if (tipo === 'repos') { verificarRepos(e.target[0].value) }
+    if (tipo === 'users') { verificarUsers(e.target[0].value) }
   }
 
   return (
     <div className="HomeContainer">
       <img src={gitlogo} alt="Github" className='GitLogo' />
-      
-      <form 
-        onSubmit={handleSubmit} 
+
+      <form
+        onSubmit={handleSubmit}
         className="HomeForm"
       >
-        {/* <div className='opcoes'>
-          <input type="radio" name="tipo" id="repos" />
-          <input type="radio" name="tipo" id="users" />
-          <input name="repos" id="tipo">Repositórios</input>
-          <input name="users" id="tipo">Usuários</input>
-        </div> */}
+        <div className='opcoes'>
+          <div
+            className='botaoTipo'
+            onClick={(e) => { e.preventDefault(); handleButtonClick('repos','users')}}
+            id='repos'
+          >
+            Repositórios
+          </div>
+          <div
+            className='botaoTipo'
+            onClick={(e) => { e.preventDefault(); handleButtonClick('users','repos')}}
+            id='users'
+          >
+            Usuários
+          </div>
+        </div>
 
         <input type="text" placeholder='Buscar...' />
 
       </form>
-    
+
     </div>
   );
 }
